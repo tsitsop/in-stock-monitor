7acf1ef9a6865f6e8fba786bc339cd6934d163e7
unknown <george@tsitsop.com>
2021-12-12 19:46:47 -0500

    changed webhook, added products

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import time
 import random
 from Product import Product
+from secrets import STOCK_ALERT_WEBHOOK
 
 headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
@@ -35,6 +36,7 @@ def loopRequest(products):
 				notify(product)
 				found[p] = True
 				time_found[p] = datetime.now()
+			time.sleep(random.randint(1, 5)) 
 
 		# semi-random wait period between every set of requests
 		time.sleep(random.randint(15, 30)) 
@@ -46,9 +48,8 @@ def loopRequest(products):
 
 def notify(product):
 	''' Send message to Discord Webhook '''
-	webhook_url = "https://discord.com/api/webhooks/919407359275720704/EDEjoOLtJtW6Ewblxjf4NJI6Ik7IkmSt92cD7I7Fa6SSOjAO_S9-eam018u8AjaO90Dc"
 	
-	webhook = DiscordWebhook(url=webhook_url)
+	webhook = DiscordWebhook(url=STOCK_ALERT_WEBHOOK)
 	embed = DiscordEmbed(title=f'''{product.name} Available!''', description=product.toNotificationText())
 	
 	webhook.add_embed(embed)
@@ -58,13 +59,18 @@ def generateProductList():
 	''' Generate the list of products that we're interested in '''
 	products = [
 		Product(
-			name="RTX 3070 FE",
-			url="https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
-		),
-		Product(
 			name="RTX 3080 FE",
 			url="https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
 		),
+		Product(
+			name="Xbox Series X",
+			url="https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324"
+		),
+		Product(
+			name="RTX 3070 FE",
+			url="https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
+		),
+		
 		Product(
 			name="RTX 3080 Ti AORUS XTREME",
 			url="https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-ti-aorus-xtreme-12gb-gddr6x-pci-express-4-0-graphics-card/6468933.p?skuId=6468933"
@@ -73,10 +79,7 @@ def generateProductList():
 			name="RTX 3080 Ti AORUS MASTER",
 			url="https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-ti-aorus-master-12gb-gddr6x-pci-express-4-0-graphics-card/6468932.p?skuId=6468932"
 		),
-		Product(
-			name="Xbox Series X",
-			url="https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324"
-		),
+		
 		Product(
 			name="PlayStation 5",
 			url="https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
